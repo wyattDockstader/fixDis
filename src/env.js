49a390485badce26589815f4a6e2457a5118b7e7
wyattDockstader <81665137+wyattDockstader@users.noreply.github.com>
2021-07-22 16:23:00 -0600

    launch

---
+++ src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'wyattd.us.auth0.com'
+export const audience = 'https://codeworks-dummy.com'
+export const clientId = 'V98CuortWhlf7oqWt2lBOHwIAfdk9UXd'
